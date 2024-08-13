eeb69021dc9986136de7fd64e88f7c24568fc6cd
Albert Cruz <albert.cruz@acpgroup.com.br>
2024-08-12 22:48:26 -0300

    force https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\Facades\Request;
 use Illuminate\Support\ServiceProvider;
 
@@ -11,7 +12,11 @@ class AppServiceProvider extends ServiceProvider
      * Register any application services.
      */
     public function register(): void
-    {
+    {   
+        if ($this->app->environment('production')) {
+            URL::forceScheme('https');
+        }
+        
         Request::macro('isAgent', function () {
             return $this->getHost() === adminUrl();
         });
